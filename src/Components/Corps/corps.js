b3c93aa9f43b98d41ab8f8364cf9d063f9d920ba
mlsnaby <mlsnaby@gmail.com>
2020-12-24 10:31:33 +0000

    share code on github

--- src/Components/Corps/corps.js
+++ src/Components/Corps/corps.js
@@ -1,138 +1,141 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {MDBIcon} from "mdbreact"  
 import p1 from "../../images/p1.jpg"
+
+import data from "../Produits/produits";
 import "./corps.css"
+import axios from 'axios';
 
 function Corps() {
+
+    /*const [produits, setProduit]= useState([])
+    const getProducts = async ()=>{
+        const {data} = await axios.get("https://gboki.com/products/all")
+        setProduit(data.products)
+        console.log(data.products)
+    }
+    useEffect(() => {
+        getProducts();
+
+    }, [])*/
+
+
     return (
-        <div className="">
+        <div className="ok">
             
               <div className="corps">
+                    <div className="corps-fex">
+                        <img src={p1} alt="produits" />
+                    </div>
+                    <div className="corps-fex">
+                        <img src={p1} alt="produits" />
+                    </div>
+                    <div className="corps-fex">
+                        <img src={p1} alt="produits" />
+                    </div>
+                    <div className="corps-fex">
+                        <img src={p1} alt="produits" />
+                    </div>
+                    <div className="corps-fex">
+                        <img src={p1} alt="produits" />
+                    </div>
+                    <div className="corps-fex">
+                        <img src={p1} alt="produits" />
+                    </div>
+                    <div className="corps-fex">
+                        <img src={p1} alt="produits" />
+                    </div>
                     <div className="corps-fex">
                         <img src={p1} alt="produits" />
                     </div>
                    
                 </div>
                 
-                
+                         {/**contenu2 */}
+                    <section>
 
-                    <div class="corps-container">
+                    <div className="titre-arrivage">
+                            <strong>Nouvel Arrivage </strong>
+                            <p>Nous vous fournissons un nouveau modele </p>
+                         </div>
 
-                        <div className="corps-box">
-                            <div className="corps-image">
-                                <a href="#" className="add-cart">
-                                <MDBIcon icon="shopping-basket" />
-                                </a>
-                                <img src={p1} alt="produits" />
-                            </div>
-                            <div className="corps-details">
-                                <a href="#" class="p-name">drawstring T-Shrt</a>
-                                <span class="p-price">2.000 CFA</span>
+                        <div class="corps-container">
+                        {/**listed des produits */}
 
-                                <div className="star-icon">
-                                    <MDBIcon icon="star"  />
-                                    <MDBIcon icon="star" />
-                                    <MDBIcon icon="star" />
+                        {
+                             /*produits.map((produit)=>(*/
+                            data.produits.map((produit)=>(
+
+                                <div className="corps-box">
+
+                                <div key={produit._id} className="corps-image">
+
+                                    <a href={produit._id} className="add-cart">
+                                    
+                                    <MDBIcon icon="shopping-baskets" />
+                                    </a>
+                                    <img src={produit.image} alt="produits" />
+                                </div>
+                                <div className="corps-details">
+
+                                    <a  href={`/produit/${produit._id}`}  href="#" class="p-name">{produit.nom}</a>
+                                    <span class="p-price">{produit.price}CFA</span>
+
+                                    <div className="star-icon">
+                                        <MDBIcon icon="star"  />
+                                        <MDBIcon icon="star" />
+                                        <MDBIcon icon="star" />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="corps-box">
-                            <div className="corps-image">
-                                <a href="#" className="add-cart">
-                                <MDBIcon icon="shopping-basket" />
-                                </a>
-                                <img src={p1} alt="produits" />
-                            </div>
-                            <div className="corps-details">
-                                <a href="#" class="p-name">drawstring T-Shrt</a>
-                                <span class="p-price">2.000 CFA</span>
+                            ))
+                        }
 
-                                <div className="star-icon">
-                                    <MDBIcon icon="star"  />
-                                    <MDBIcon icon="star" />
-                                    <MDBIcon icon="star" />
+                        </div>
+                    </section>
+                     
+                     {/**contenu2 */}
+
+                     <section>
+                        <div className="titre-arrivage">
+                            <strong>Future Arrivage</strong>
+                            <p>Nous vous fournissons un nouveau modele </p>
+                         </div>
+
+                            <div class="corps-container">
+                        {/**listed des produits contenu 2 */}
+
+                        {
+                            data.produits.map((produit)=>(
+
+                                <div className="corps-box">
+
+                                <div key={produit._id} className="corps-image">
+
+                                    <a href={`${produit._id}`} className="add-cart">
+                                    
+                                    <MDBIcon icon="shopping-baskets" />
+                                    </a>
+                                    <img src={`${produit.image}`} alt="produits" />
+                                </div>
+                                <div className="corps-details">
+
+                                    <a  href={`/produit/${produit._id}`}  href="#" class="p-name">{produit.nom}</a>
+                                    <span class="p-price">{produit.price}CFA</span>
+
+                                    <div className="star-icon">
+                                        <MDBIcon icon="star"  />
+                                        <MDBIcon icon="star" />
+                                        <MDBIcon icon="star" />
+                                    </div>
                                 </div>
                             </div>
+                            ))
+                        }
+
                         </div>
 
-                        <div className="corps-box">
-                            <div className="corps-image">
-                                <a href="#" className="add-cart">
-                                <MDBIcon icon="shopping-basket" />
-                                </a>
-                                <img src={p1} alt="produits" />
-                            </div>
-                            <div className="corps-details">
-                                <a href="#" class="p-name">drawstring T-Shrt</a>
-                                <span class="p-price">2.000 CFA</span>
-
-                                <div className="star-icon">
-                                    <MDBIcon icon="star"  />
-                                    <MDBIcon icon="star" />
-                                    <MDBIcon icon="star" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="corps-box">
-                            <div className="corps-image">
-                                <a href="#" className="add-cart">
-                                <MDBIcon icon="shopping-basket" />
-                                </a>
-                                <img src={p1} alt="produits" />
-                            </div>
-                            <div className="corps-details">
-                                <a href="#" class="p-name">drawstring T-Shrt</a>
-                                <span class="p-price">2.000 CFA</span>
-
-                                <div className="star-icon">
-                                    <MDBIcon icon="star"  />
-                                    <MDBIcon icon="star" />
-                                    <MDBIcon icon="star" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="corps-box">
-                            <div className="corps-image">
-                                <a href="#" className="add-cart">
-                                <MDBIcon icon="shopping-basket" />
-                                </a>
-                                <img src={p1} alt="produits" />
-                            </div>
-                            <div className="corps-details">
-                                <a href="#" class="p-name">drawstring T-Shrt</a>
-                                <span class="p-price">2.000 CFA</span>
-
-                                <div className="star-icon">
-                                    <MDBIcon icon="star"  />
-                                    <MDBIcon icon="star" />
-                                    <MDBIcon icon="star" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="corps-box">
-                            <div className="corps-image">
-                                <a href="#" className="add-cart">
-                                <MDBIcon icon="shopping-basket" />
-                                </a>
-                                <img src={p1} alt="produits" />
-                            </div>
-                            <div className="corps-details">
-                                <a href="#" class="p-name">drawstring T-Shrt</a>
-                                <span class="p-price">2.000 CFA</span>
-
-                                <div className="star-icon">
-                                    <MDBIcon icon="star"  />
-                                    <MDBIcon icon="star" />
-                                    <MDBIcon icon="star" />
-                                </div>
-                            </div>
-                        </div>
-
-                        
-                     </div>
+                     </section>
                      
 
                 
---
+++ src/Components/Produits/produits.js
@@ -0,0 +1,85 @@
+import p1 from '../../images/p1.jpg'
+import p2 from '../../images/p2.jpg'
+import p3 from '../../images/p3.jpg'
+import p4 from '../../images/p4.jpg'
+import p5 from '../../images/p5.jpg'
+import p6 from '../../images/p6.jpg'
+import p7 from '../../images/p7.jpg'
+import p8 from '../../images/p8.jpg'
+import p9 from '../../images/p9.jpg'
+import p10 from '../../images/p10.jpg'
+import p11 from '../../images/p11.jpg'
+import p12 from '../../images/p12.jpg'
+
+const data = {
+    produits:[
+        {
+            _id:'1',
+            nom: 'pull over slim',
+            categories: 'Pull',
+            image: p1,
+            price: 200,
+            brand: 'FREE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality produit'
+        },
+        {
+            _id:'2',
+            nom: '2pull over slim 2',
+            categories: 'Pull',
+            image: p2,
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality produit'
+        },
+        {
+            _id:'3',
+            nom: '3 pull over slim3',
+            categories: 'Pull',
+            image: p3,
+            price: 3000,
+            brand: 'ADIDAS',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality produit'
+        },
+        {
+            _id:'4',
+            nom: '4 pull over slim',
+            categories: 'Pull',
+            image: p4,
+            price: 1200,
+            brand: 'PUMA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality produit'
+        },
+        {
+            _id:'1',
+            nom: 'pull over slim',
+            categories: 'Pull',
+            image: p5,
+            price: 150,
+            brand: 'FREE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality produit'
+        },
+        {
+            _id:'2',
+            nom: '2pull over slim 2',
+            categories: 'Pull',
+            image: p6,
+            price: 8000,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality produit'
+        }
+    ],
+};
+
+export default data;
